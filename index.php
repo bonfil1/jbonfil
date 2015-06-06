18af6bd833873ca8f820c6aa2da217c944d41d5e
Jose A. Bonfil <jose.bonfil@bairesdev.com>
2015-06-06 14:32:32 -0500

    First commit of my first homepage :)

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP</title>
+
+        <link href="static/css/site.css" type="text/css" />
+    </head>
+    <body>
+        <header></header>
+
+        <div class="wrapper">
+            <h1>Hola</h1>
+        </div>
+
+        <footer></footer>
+    </body>
+</html>
