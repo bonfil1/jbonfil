f3dbf839bd7ec743f3bded90a943ae04c733892e
Jose A. Bonfil <jose.bonfil@bairesdev.com>
2015-06-13 14:05:38 -0500

    new blog styles added to the current site

--- index.php
+++ index.php
@@ -1,21 +1,53 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Jose Angel Bonfil Evia</title>
+        <title>Jose Angel Bonfil Evia | Website</title>
 
-        <link rel="stylesheet" href="static/css/site.css" type="text/css" />
+        <link href="/static/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+        <link href="/static/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+        <!--[if IE]>
+            <link href="/static/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+        <![endif]-->
     </head>
     <body>
         <header>
-
+            <div class="inner-header">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">CV</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </header>
 
         <div class="wrapper">
-            <h1>Site under development</h1>
+            <h1>Jose Angel Bonfil Evia Site</h1>
+
+            <div class="col-left">
+                <!-- Posts section -->
+
+                <div class="post img full">
+                    <img src="http://i.4cdn.org/wg/1433309373027.jpg" alt="Blog post">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed orci ac ligula aliquam sollicitudin. Quisque accumsan lacus vitae nulla dictum vulputate. Integer ac magna lorem. Nulla sit amet accumsan turpis. Suspendisse sed metus eu tortor laoreet luctus. Sed tempus sed nibh sagittis finibus. Donec non commodo velit. Nullam a imperdiet nibh. Nulla sodales nisl nec felis varius consequat.</p>
+                    <a href="#" class="blog-entry">Read more</a>
+                </div>
+
+                <div class="post img full">
+                    <img src="http://i.4cdn.org/wg/1433427056864.jpg" alt="Blog post">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed orci ac ligula aliquam sollicitudin. Quisque accumsan lacus vitae nulla dictum vulputate. Integer ac magna lorem. Nulla sit amet accumsan turpis. Suspendisse sed metus eu tortor laoreet luctus. Sed tempus sed nibh sagittis finibus. Donec non commodo velit. Nullam a imperdiet nibh. Nulla sodales nisl nec felis varius consequat.</p>
+                    <a href="#" class="blog-entry">Read more</a>
+                </div>
+            </div>
+            <div class="col-right">
+
+            </div>
         </div>
 
         <footer>
-
+            <div class="inner-footer">
+                <p>&copy; 2015 - Jose A. Bonfil Evia | All Rights Reserved.</p>
+            </div>
         </footer>
     </body>
 </html>
